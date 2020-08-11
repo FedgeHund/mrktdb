ea06ea29bd2bd477582d0c155b4f6f6618cc1df2
pushpitbhardwaj <pushpit07@gmail.com>
2020-08-11 21:33:28 +0530

    Watchlist

--- fedgehund_profile/views.py
+++ fedgehund_profile/views.py
@@ -3,9 +3,11 @@ from rest_framework.views import APIView
 from .serializers import WatchlistSerializer
 from .models import Watchlist
 
+
 class WatchlistView(APIView):
     def get(self, request):
     	user = self.request.user
     	companies = Watchlist.objects.filter(user=user)
     	serializer = WatchlistSerializer(companies, many=True)
     	return Response({"watchlist": serializer.data})
+
--- fedgehundapi/urls.py
+++ fedgehundapi/urls.py
@@ -20,4 +20,9 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('fedgehund_auth.urls')),
+
+    path('profile/', include('fedgehund_profile.urls')),
+    #to rectify 'NoReverseMatch at /auth/registration/' issue
+    re_path(r'^account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),
+	re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$', VerifyEmailView.as_view(), name='account_confirm_email'),
 ]

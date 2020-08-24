4a21fcd5beecb4f8a61616b662dabefdb7649c44
pushpitbhardwaj <pushpit07@gmail.com>
2020-08-24 16:48:32 +0530

    Commiting before starting with the frontend

--- fedgehund_profile/urls.py
+++ fedgehund_profile/urls.py
@@ -6,5 +6,5 @@ app_name = "Company_watchlist"
 
 # app_name will help us do a reverse look-up latter.
 urlpatterns = [
-    path('company_watchlist/', WatchlistView.as_view()),
+    path('watchlist/', WatchlistView.as_view()),
 ]

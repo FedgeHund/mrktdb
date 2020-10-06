81dad00c801dd20e29e5ce76c134f32277c82776
pushpitbhardwaj <pushpit07@gmail.com>
2020-10-06 22:47:47 +0530

    Changed ENVIRON variable to dev

--- fedgehundui/src/components/App.js
+++ fedgehundui/src/components/App.js
@@ -21,7 +21,7 @@ class App extends Component {
 }
 
 const ENV_VARIABLES = {
-  ENVIRON: "prod",
+  ENVIRON: "dev",
   DEV_API_URL: "127.0.0.1:8000",
   PROD_API_URL: "mrktdbapi-prod.eba-tw27jjhn.us-west-2.elasticbeanstalk.com",
 };

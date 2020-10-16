cb98f508960d9e307e890061597246541db21943
pushpitbhardwaj <pushpit07@gmail.com>
2020-10-16 17:16:00 +0530

    Made changes to the cronJob so that it gets the object for the latest year

--- edgar/securityLinkCompanyCronJob.py
+++ edgar/securityLinkCompanyCronJob.py
@@ -5,7 +5,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fedgehundapi.settings')
 from edgar.models import CikCusipMapping, Security, Company
 
 class SecurityLinkCompanyCronJob(CronJobBase):
-    RUN_EVERY_MINS = 1 # we dont run this every minute, we will be using linux crontab to setup when the job will run
+    RUN_EVERY_MINS = 0.01 # we dont run this every minute, we will be using linux crontab to setup when the job will run
 
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'edgar.securityLinkCompanyCronJob'    # a unique code
@@ -13,10 +13,10 @@ class SecurityLinkCompanyCronJob(CronJobBase):
     def do(self):
         security_items = Security.objects.all()
         company_items = Company.objects.all()
-        cikCusipMapping_items = CikCusipMapping.objects.all()
+        cikCusipMapping_items = CikCusipMapping.objects.order_by('year').all()
 
-        for security in security_items:
-            for mapping in cikCusipMapping_items:
+        for mapping in cikCusipMapping_items:
+            for security in security_items:
                 if(security.cusip == mapping.cusip or security.cusip == mapping.cusip6):
                     for company in company_items:
                         if(company.cik == mapping.cik):

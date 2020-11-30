0103a165d0a1921bdd601fff29f0eb61c16039df
Pushpit <pushpit07@gmail.com>
2020-11-30 13:57:47 +0530

    Update views.py

--- fedgehund_profile/views.py
+++ fedgehund_profile/views.py
@@ -1,10 +1,10 @@
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 from .serializers import WatchlistSerializer
 from .serializers import ProfileSerializer
 from .models import Watchlist, Profile
-from rest_framework import status
 
 class ProfileView(ListCreateAPIView):
 	serializer_class = ProfileSerializer

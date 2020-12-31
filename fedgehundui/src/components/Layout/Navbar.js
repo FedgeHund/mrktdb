9f9730ec54092d6176245004abdfa928c3f8d0b1
pushpitbhardwaj <pushpit07@gmail.com>
2020-12-31 10:51:41 +0530

    Animated navbar menu

--- fedgehundui/src/components/Layout/Navbar.js
+++ fedgehundui/src/components/Layout/Navbar.js
@@ -53,45 +53,47 @@ function Navbar() {
 						<div className="hamburger_container">
 							<div className="menu_box">
 								<div className="row margin_small15">
-									<div className="col-md-1 col-sm-2 col-4 margin_small10">
+									<div className="col-md-1 col-sm-2 col-4 margin_small10 logo_div">
 										<img src="../../../static/homepage/logo2.png" alt="White logo" className="hamburger_white_logo"/>
 									</div>
 									<div className="col-md-1 mrktdb_white col-2 margin_small10">
 										MrktDB
 									</div>
-									<div className="offset-md-5 col-md-2 offset-sm-2 col-sm-2 col-4 margin_small10">
+									<div className="offset-md-5 col-md-2 offset-sm-2 col-sm-2 col-4 margin_small10 signup_left">
 										<a href="/signup" className="signup_menu">Sign Up</a>
 									</div>
-									<div className="col-md-2 col-sm-2 col-4 margin_small10">
+									<div className="col-md-2 col-sm-2 col-4 margin_small10 signup_left">
 										<a href="/signin" className="signup_menu">Sign In</a>
 									</div>
 								</div>
 
-								<div className="row margin10">
-									<div className="offset-md-2 col-md-1 menu_13f margin3">13F</div>
+								<div className="up_13f">
+									<div className="row margin10">
+										<div className="offset-md-2 col-md-1 menu_13f margin3">13F</div>
+									</div>
+
+									<div className="row hover_effect_row margin6">
+										<a href="#" className="filer_items offset-md-2 col-md-2 col-12 hover_effect"><div className="margin_small6">Latest Filings&nbsp; <i className="fas fa-arrow-right link_arrow"></i></div></a>
+										<a href="#" className="filer_items col-md-4 col-12 hover_effect"><div className="margin_small3">13F Searching&nbsp; <i className="fas fa-arrow-right link_arrow"></i></div></a>
+									</div>
+
+									<div className="row hover_effect_row">
+										<a href="#" className="filer_items offset-md-2 col-md-2 col-12 hover_effect"><div className="margin_small3">Popular Stocks&nbsp; <i className="fas fa-arrow-right link_arrow"></i></div></a>
+										<a href="#" className="filer_items col-md-4 col-12 hover_effect"><div className="margin_small3">13F Fund Performance Search&nbsp; <i className="fas fa-arrow-right link_arrow"></i></div></a>
+									</div>
+
+									<div className="row hover_effect_row">
+										<a href="#" className="filer_items offset-md-2 col-md-2 col-12 hover_effect"><div className="margin_small3">13F Trends&nbsp; <i className="fas fa-arrow-right link_arrow"></i></div></a>
+										<a href="#" className="filer_items col-md-4 col-12 hover_effect"><div className="margin_small3">13F Stock Screener&nbsp; <i className="fas fa-arrow-right link_arrow"></i></div></a>
+									</div>
+
+									<div className="row hover_effect_row">
+										<a href="#" className="filer_items offset-md-2 col-md-3 col-12 hover_effect"><div className="margin_small3">13F Statistics&nbsp; <i className="fas fa-arrow-right link_arrow"></i></div></a>              
+									</div>
 								</div>
 
 								<div className="row">
-									<a href="#" className="filer_items offset-md-2 col-md-2 col-12"><div className="margin6 margin_small6">Latest Filings</div></a>
-									<a href="#" className="filer_items col-md-4 col-12"><div className="margin6 margin_small3">13F Searching</div></a>
-								</div>
-
-								<div className="row">
-									<a href="#" className="filer_items offset-md-2 col-md-2 col-12"><div className="margin3 margin_small3">Popular Stocks</div></a>
-									<a href="#" className="filer_items col-md-4 col-12"><div className="margin3 margin_small3">13F Fund Performance Search</div></a>
-								</div>
-
-								<div className="row">
-									<a href="#" className="filer_items offset-md-2 col-md-2 col-12"><div className="margin3 margin_small3">13F Trends</div></a>
-									<a href="#" className="filer_items col-md-4 col-12"><div className="margin3 margin_small3">13F Stock Screener</div></a>
-								</div>
-
-								<div className="row">
-									<a href="#" className="filer_items offset-md-2 col-md-3 col-12"><div className="margin3 margin_small3">13F Statistics</div></a>              
-								</div>
-
-								<div className="row">
-									<div className="horizontal_line margin10 offset-md-2 col-md-8 col-12 mb-2"></div>
+									<div className="horizontal_line margin10 offset-md-2 col-md-8 col-12 mb-2 horizontal_line_to_right"></div>
 								</div>
 
 
@@ -104,32 +106,32 @@ function Navbar() {
 										<div className="footer_heading col-2">Explore</div>
 									</div>
 
-									<div className="row">
-										<a href="#" className="filer_items offset-2 col-2"><div className="margin6">Our offerings</div></a>
-										<a href="#" className="filer_items col-2"><div className="margin6">Advertise with us</div></a>
-										<a href="#" className="filer_items col-2"><div className="margin6">Contact Us</div></a>
-										<a href="#" className="filer_items col-2"><div className="margin6">Email Newsletter</div></a>
+									<div className="row hover_effect_row margin6">
+										<a href="#" className="filer_items offset-2 col-2 hover_effect"><div>Our offerings&nbsp; <i className="fas fa-arrow-right link_arrow"></i></div></a>
+										<a href="#" className="filer_items col-2 hover_effect"><div>Advertise with us&nbsp; <i className="fas fa-arrow-right link_arrow"></i></div></a>
+										<a href="#" className="filer_items col-2 hover_effect"><div>Contact Us&nbsp; <i className="fas fa-arrow-right link_arrow"></i></div></a>
+										<a href="#" className="filer_items col-2 hover_effect"><div>Email Newsletter&nbsp; <i className="fas fa-arrow-right link_arrow"></i></div></a>
 									</div>
 
-									<div className="row">
-										<a href="#" className="filer_items offset-2 col-2"><div className="margin3">How MrktDB works?</div></a>
-										<a href="#" className="filer_items col-2"><div className="margin3">Business Resources</div></a>
-										<a href="#" className="filer_items col-2"><div className="margin3">Report a Bug</div></a>
-										<a href="#" className="filer_items col-2"><div className="margin3">Upcoming events</div></a>
+									<div className="row hover_effect_row">
+										<a href="#" className="filer_items offset-2 col-2 hover_effect"><div>How MrktDB works?&nbsp; <i className="fas fa-arrow-right link_arrow"></i></div></a>
+										<a href="#" className="filer_items col-2 hover_effect"><div>Business Resources&nbsp; <i className="fas fa-arrow-right link_arrow"></i></div></a>
+										<a href="#" className="filer_items col-2 hover_effect"><div>Report a Bug&nbsp; <i className="fas fa-arrow-right link_arrow"></i></div></a>
+										<a href="#" className="filer_items col-2 hover_effect"><div>Upcoming events&nbsp; <i className="fas fa-arrow-right link_arrow"></i></div></a>
 									</div>
 
-									<div className="row">
-										<a href="#" className="filer_items offset-2 col-2"><div className="margin3">Getting started</div></a>
-										<a href="#" className="filer_items col-2"><div className="margin3"></div></a>
-										<a href="#" className="filer_items col-2"><div className="margin3">Sign in</div></a>
-										<a href="#" className="filer_items col-2"><div className="margin3"></div></a>
+									<div className="row hover_effect_row">
+										<a href="#" className="filer_items offset-2 col-2 hover_effect"><div>Getting started&nbsp; <i className="fas fa-arrow-right link_arrow"></i></div></a>
+										<a href="#" className="filer_items col-2 hover_effect"><div></div></a>
+										<a href="#" className="filer_items col-2 hover_effect"><div>Sign in&nbsp; <i className="fas fa-arrow-right link_arrow"></i></div></a>
+										<a href="#" className="filer_items col-2 hover_effect"><div></div></a>
 									</div>
 
-									<div className="row">
-										<a href="#" className="filer_items offset-2 col-2"><div className="margin3">FAQ</div></a>
-										<a href="#" className="filer_items col-2"><div className="margin3"></div></a>
-										<a href="#" className="filer_items col-2"><div className="margin3"></div></a>
-										<a href="#" className="filer_items col-2"><div className="margin3"></div></a>
+									<div className="row hover_effect_row">
+										<a href="#" className="filer_items offset-2 col-2 hover_effect"><div>FAQ&nbsp; <i className="fas fa-arrow-right link_arrow"></i></div></a>
+										<a href="#" className="filer_items col-2 hover_effect"><div></div></a>
+										<a href="#" className="filer_items col-2 hover_effect"><div></div></a>
+										<a href="#" className="filer_items col-2 hover_effect"><div></div></a>
 									</div>
 								</div>
 
@@ -138,7 +140,7 @@ function Navbar() {
 										<div className="col-12 footer_section">Quick links</div>
 									</div>
 									<div className="row">
-										<div className="horizontal_lines_small_screen col-12 mt-2"></div>
+										<div className="horizontal_lines_small_screen col-12 mt-2 horizontal_lines_small_screen_to_right"></div>
 									</div>
 
 									<div className="row margin_small10">
@@ -147,22 +149,22 @@ function Navbar() {
 									</div>
 
 									<div className="row margin_small6">
-										<a href="#" className="filer_items col-6"><div>Our offerings</div></a>
-										<a href="#" className="filer_items col-6"><div>Contact Us</div></a>
+										<a href="#" className="filer_items col-6 hover_effect"><div>Our offerings</div></a>
+										<a href="#" className="filer_items col-6 hover_effect"><div>Contact Us</div></a>
 									</div>
 
 									<div className="row margin_small3">
-										<a href="#" className="filer_items col-6"><div>How MrktDB works?</div></a>
-										<a href="#" className="filer_items col-6"><div>Report a Bug</div></a>
+										<a href="#" className="filer_items col-6 hover_effect"><div>How MrktDB works?</div></a>
+										<a href="#" className="filer_items col-6 hover_effect"><div>Report a Bug</div></a>
 									</div>
 
 									<div className="row margin_small3">
-										<a href="#" className="filer_items col-6"><div>Getting started</div></a>
-										<a href="#" className="filer_items col-6"><div>Sign in</div></a>
+										<a href="#" className="filer_items col-6 hover_effect"><div>Getting started</div></a>
+										<a href="#" className="filer_items col-6 hover_effect"><div>Sign in</div></a>
 									</div>
 
 									<div className="row margin_small3">
-										<a href="#" className="filer_items col-6"><div>FAQ</div></a>
+										<a href="#" className="filer_items col-6 hover_effect"><div>FAQ</div></a>
 									</div>
 
 
@@ -172,13 +174,13 @@ function Navbar() {
 									</div>
 
 									<div className="row margin_small6">
-										<a href="#" className="filer_items col-6"><div>Advertise with us</div></a>
-										<a href="#" className="filer_items col-6"><div>Email Newsletter</div></a>
+										<a href="#" className="filer_items col-6 hover_effect"><div>Advertise with us</div></a>
+										<a href="#" className="filer_items col-6 hover_effect"><div>Email Newsletter</div></a>
 									</div>
 
 									<div className="row margin_small3">
-										<a href="#" className="filer_items col-6"><div>Business Resources</div></a>
-										<a href="#" className="filer_items col-6"><div>Upcoming events</div></a>
+										<a href="#" className="filer_items col-6 hover_effect"><div>Business Resources</div></a>
+										<a href="#" className="filer_items col-6 hover_effect"><div>Upcoming events</div></a>
 									</div>
 
 
@@ -188,19 +190,19 @@ function Navbar() {
 									</div>               
 
 									<div className="row margin_small6">
-										<a href="#" className="filer_items col-6"><div>Twitter</div></a>
+										<a href="#" className="filer_items col-6 hover_effect"><div>Twitter</div></a>
 									</div>
 
 									<div className="row margin_small3">
-										<a href="#" className="filer_items col-6"><div>Instagram</div></a>
+										<a href="#" className="filer_items col-6 hover_effect"><div>Instagram</div></a>
 									</div>
 
 									<div className="row margin_small3">
-										<a href="#" className="filer_items col-6"><div>Facebook</div></a>
+										<a href="#" className="filer_items col-6 hover_effect"><div>Facebook</div></a>
 									</div>
 
 									<div className="row margin_small3">
-										<a href="#" className="filer_items col-6"><div>LinkedIn</div></a>
+										<a href="#" className="filer_items col-6 hover_effect"><div>LinkedIn</div></a>
 									</div>
 
 									<div className="row margin_small10">

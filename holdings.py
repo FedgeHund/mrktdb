3c825071d26a9e6ce735b17106bd5943ed79f5a3
shivanshmehendiratta <shivansh.19521@sscbs.du.ac.in>
2021-03-22 00:27:38 +0530

    bulk save

--- holdings.py
+++ holdings.py
@@ -24,7 +24,9 @@ quarterlyothermanagers = QuarterlyOtherManager.objects.all()
 
 for filer in filers:
     quarterlyholdings = QuarterlyHolding.objects.filter(filerId=filer)
+    arrr = []
     for quarterlyholding in quarterlyholdings:
+        arr = {}
         quarterlysecurityholdings = QuarterlySecurityHolding.objects.filter(quarterlyHoldingId=quarterlyholding)
         previous_quarterlysecurityholdings = QuarterlySecurityHolding.objects.filter(quarterlyHoldingId=next_in_order(quarterlyholding))
         total_market_value = 0
@@ -40,8 +42,9 @@ for filer in filers:
             weight_percent = (quarterlysecurityholding.marketvalue/total_market_value)*100
 
             try:
-                if(QuarterlySecurityHolding.objects.filter(quarterlyHoldingId=next_in_order(quarterlyholding),securityId = security_id).first()!=None):
-                    previous_weight_percent = (QuarterlySecurityHolding.objects.filter(quarterlyHoldingId=next_in_order(quarterlyholding),securityId = security_id).first().marketvalue/previous_total_market_value)*100
+                qq = QuarterlySecurityHolding.objects.filter(quarterlyHoldingId=next_in_order(quarterlyholding),securityId = security_id).first()
+                if(qq!=None):
+                    previous_weight_percent = (qq.marketvalue/previous_total_market_value)*100
             except:
                 print("Error while calculating previous weight percent")
                 pass
@@ -53,8 +56,7 @@ for filer in filers:
             position_type = ''
             position_change = 0
             quarterlysecurityholdings_for_filer_for_security=[]
-            quarterlyholdingobjects = QuarterlyHolding.objects.filter(filerId = filer)
-            for quarterlyholdingobject in quarterlyholdingobjects:
+            for quarterlyholdingobject in quarterlyholdings:
                 quarterlysecurityholding_for_filer_for_security = QuarterlySecurityHolding.objects.filter(securityId = security_id, quarterlyHoldingId = quarterlyholdingobject).first()
                 if (quarterlysecurityholding_for_filer_for_security!=None):
                     quarterlysecurityholdings_for_filer_for_security.append(quarterlysecurityholding_for_filer_for_security)
@@ -103,15 +105,67 @@ for filer in filers:
             price = None
             
             try:
-                if(Price.objects.filter(name=security_id.securityName,quarter=quarterlyholding.quarter).first()):
-                    price = Price.objects.filter(name=security_id.securityName,quarter=quarterlyholding.quarter).first().value
+                priceobject = Price.objects.filter(name=security_id.securityName,quarter=quarterlyholding.quarter).first()
+                if(priceobject):
+                    arr['lastPrice'] = priceobject.value
                     print(price)
             except:
                 print('price for security not available')
                 pass
+            arr['quarter'] = quarterlyholding.quarter
+            arr['filerId'] = filer
+            arr['securityName'] = security_id.securityName
+            arr['filerName'] = quarterlyholding.filerId.companyId.name
+            arr['securityId']=security_id
+            arr['quarterId']=quarterlyholding
+            arr['investmentDiscretion']=quarterlysecurityholding.investmentDiscretion
+            arr['marketValue']=quarterlysecurityholding.marketvalue
+            arr['quantity']=quarterlysecurityholding.quantity
+            arr['weightPercent']=weight_percent 
+            arr['changeInShares'] = change_in_shares
+            arr['positionType'] = position_type
+            arr['previousWeightPercent'] = previous_weight_percent
+            arr['changeInPositionPercent'] = position_change
+            arr['sourceType'] = filer.fileType
+            arr['sourcedOn']=quarterlyholding.filedOn
             if(qtr_first_owned!=None):
-                position = Position(lastPrice=price,quarter=quarterlyholding.quarter,securityName=security_id.securityName,filerName=quarterlyholding.filerId.companyId.name,securityId=security_id,quarterId=quarterlyholding,investmentDiscretion=quarterlysecurityholding.investmentDiscretion,marketValue=quarterlysecurityholding.marketvalue,quantity=quarterlysecurityholding.quantity, weightPercent=weight_percent, changeInShares = change_in_shares, positionType = position_type, previousWeightPercent = previous_weight_percent, changeInPositionPercent = position_change, sourceType = filer.fileType, sourcedOn=quarterlyholding.filedOn, quarterFirstOwned=qtr_first_owned)
-            else:
-                position = Position(lastPrice=price,quarter=quarterlyholding.quarter,securityName=security_id.securityName,filerName=quarterlyholding.filerId.companyId.name,securityId=security_id,quarterId=quarterlyholding,investmentDiscretion=quarterlysecurityholding.investmentDiscretion,marketValue=quarterlysecurityholding.marketvalue,quantity=quarterlysecurityholding.quantity, weightPercent=weight_percent, changeInShares = change_in_shares, positionType = position_type, previousWeightPercent = previous_weight_percent, changeInPositionPercent = position_change, sourceType = filer.fileType, sourcedOn=quarterlyholding.filedOn)
-            position.save()
+                arr['quarterFirstOwned']=qtr_first_owned
+            arrr.append(arr)
+            
+    try:
+        PositionObjectstbs = [
+            Position(
+                quarter = arrr[i]['quarter'],
+                filerId = arrr[i]['filerId'],
+                securityName = arrr[i]['securityName'],
+                filerName = arrr[i]['filerName'],
+                securityId = arrr[i]['securityId'],
+                quarterId = arrr[i]['quarterId'],
+                investmentDiscretion = arrr[i]['investmentDiscretion'],
+                marketValue = arrr[i]['marketValue'],
+                quantity = arrr[i]['quantity'],
+                weightPercent = arrr[i]['weightPercent'],
+                changeInShares = arrr[i]['changeInShares'],
+                positionType = arrr[i]['positionType'],
+                previousWeightPercent = arrr[i]['previousWeightPercent'],
+                changeInPositionPercent = arrr[i]['changeInPositionPercent'],
+                sourceType = arrr[i]['sourceType'],
+                sourcedOn = arrr[i]['sourcedOn']
+            )for i in range(len(arrr))
+        ]
+    except:
+        print("Exception caught while creating PositionObjectstbs list")
+        pass
+    print("Bulk Save for Position Started")
+    try:
+        Position.objects.bulk_create(PositionObjectstbs)
+    except:
+        print("Exception caught while saving Position")
+        pass
+    print("Bulk Save for Position Ended")    
+            # if(qtr_first_owned!=None):
+            #     position = Position(lastPrice=price,quarter=quarterlyholding.quarter,securityName=security_id.securityName,filerName=quarterlyholding.filerId.companyId.name,securityId=security_id,quarterId=quarterlyholding,investmentDiscretion=quarterlysecurityholding.investmentDiscretion,marketValue=quarterlysecurityholding.marketvalue,quantity=quarterlysecurityholding.quantity, weightPercent=weight_percent, changeInShares = change_in_shares, positionType = position_type, previousWeightPercent = previous_weight_percent, changeInPositionPercent = position_change, sourceType = filer.fileType, sourcedOn=quarterlyholding.filedOn, quarterFirstOwned=qtr_first_owned)
+            # else:
+            #     position = Position(lastPrice=price,quarter=quarterlyholding.quarter,securityName=security_id.securityName,filerName=quarterlyholding.filerId.companyId.name,securityId=security_id,quarterId=quarterlyholding,investmentDiscretion=quarterlysecurityholding.investmentDiscretion,marketValue=quarterlysecurityholding.marketvalue,quantity=quarterlysecurityholding.quantity, weightPercent=weight_percent, changeInShares = change_in_shares, positionType = position_type, previousWeightPercent = previous_weight_percent, changeInPositionPercent = position_change, sourceType = filer.fileType, sourcedOn=quarterlyholding.filedOn)
+            # position.save()
 

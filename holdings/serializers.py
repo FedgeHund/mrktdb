8b83519704365cebc5928b5b504ae40b2bc46a33
Pushpit <pushpit07@gmail.com>
2021-04-01 22:08:29 +0530

    Updated serializers for positions/
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- holdings/serializers.py
+++ holdings/serializers.py
@@ -4,4 +4,4 @@ from .models import Position
 class PositionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Position
-        fields = ['quarter','securityName','filerName','securityId','quarterId','quarterFirstOwned','investmentDiscretion','quantity','marketValue','weightPercent','previousWeightPercent','estimatedAverageCost','lastPrice','estimatedProfit','ranking','changeInShares','changeInPositionPercent','sourceType','sourcedOn','positionType','positionId','createdAt','updatedAt','deletedAt']
+        fields = ['quarter','securityName','filerName','investmentDiscretion','quantity','marketValue','weightPercent','previousWeightPercent','estimatedAverageCost','lastPrice','estimatedProfit','ranking','changeInShares','changeInPositionPercent','sourceType','sourcedOn','positionType','positionId','createdAt','updatedAt','deletedAt']

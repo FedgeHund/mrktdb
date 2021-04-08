213fbe3fc1dad6bc2b6a4f1d1a60ffec3bd1f7c1
Pushpit <pushpit07@gmail.com>
2021-04-08 19:05:28 +0530

    Applied pagination to holdings
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- holdings/views.py
+++ holdings/views.py
@@ -1,10 +1,16 @@
 from .models import Position
 from holdings.serializers import PositionSerializer
-from rest_framework import generics
+from rest_framework import generics, filters
+from rest_framework.pagination import PageNumberPagination
 
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 100
+    page_size_query_param = 'page_size'
+    max_page_size = 200
 
-class PositionList(generics.ListCreateAPIView):
+class PositionList(generics.ListAPIView):
     queryset = Position.objects.all()
+    pagination_class = StandardResultsSetPagination
     serializer_class = PositionSerializer
 
 class PositionDetail(generics.RetrieveUpdateDestroyAPIView):

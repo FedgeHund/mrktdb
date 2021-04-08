3f4cc282dd0dd23e39a85cdd61e81eba57a958dd
Pushpit <pushpit07@gmail.com>
2021-04-08 09:24:02 +0530

    updated filerview url by /
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- filer/urls.py
+++ filer/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from filer import views
 
 urlpatterns = [
-    path('quarterlyfiler', views.QuarterlyFilerViewList.as_view()),
+    path('quarterlyfiler/', views.QuarterlyFilerViewList.as_view()),
     path('quarterlyfiler/<int:pk>/', views.QuarterlyFilerViewDetail.as_view()),
 ]
 

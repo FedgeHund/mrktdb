dbf067543e05d79637568f86fccf4abe20ca623f
Pushpit <pushpit07@gmail.com>
2021-04-08 09:06:26 +0530

    Added / to positions url

--- holdings/urls.py
+++ holdings/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from holdings import views
 
 urlpatterns = [
-    path('positions', views.PositionList.as_view()),
+    path('positions/', views.PositionList.as_view()),
     path('positions/<int:pk>/', views.PositionDetail.as_view()),
 ]
 

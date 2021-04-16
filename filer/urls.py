a7a5c6ace2b3f97cab205e4201565234367b06a8
Pushpit <pushpit07@gmail.com>
2021-04-16 17:31:43 +0530

    13F summary table and market value chart views in quarterlyfiler
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- filer/urls.py
+++ filer/urls.py
@@ -5,6 +5,8 @@ from filer import views
 urlpatterns = [
     path('quarterlyfiler/', views.QuarterlyFilerViewList.as_view()),
     path('quarterlyfiler/<int:pk>/', views.QuarterlyFilerViewDetail.as_view()),
+    path('summarytable13f/', views.LatestQuarterData13FSummaryTable.as_view()),
+    path('quarterlymarketvaluechart/', views.QuarterlyMarketValueChart.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- filer/views.py
+++ filer/views.py
@@ -1,9 +1,38 @@
 from filer.models import QuarterlyFilerView
 from .serializers import QuarterlyFilerViewSerializer
-from rest_framework import generics
+from rest_framework import generics, filters
 
+
+class LatestQuarterData13FSummaryTable(generics.ListAPIView):
+    serializer_class = QuarterlyFilerViewSerializer
+
+    def get_queryset(self):
+        if self.request.method == 'GET':
+            queryset = QuarterlyFilerView.objects.all()
+            cik = self.request.GET.get('cik', None)
+            if cik is not None:
+                queryset = queryset.filter(cik=cik).order_by('-quarter')
+                latest_quarter = queryset[0].quarter
+                queryset = queryset.filter(quarter=latest_quarter)[:1]
+                print(queryset.query)
+                
+            return queryset
+
+class QuarterlyMarketValueChart(generics.ListAPIView):
+    serializer_class = QuarterlyFilerViewSerializer
+
+    def get_queryset(self):
+        if self.request.method == 'GET':
+            queryset = QuarterlyFilerView.objects.all()
+            cik = self.request.GET.get('cik', None)
+            if cik is not None:
+                queryset = queryset.filter(cik=cik).order_by('quarter')
+                
+            return queryset
 
 class QuarterlyFilerViewList(generics.ListCreateAPIView):
+    search_fields = ['^cik']
+    filter_backends = (filters.SearchFilter,)
     queryset = QuarterlyFilerView.objects.all()
     serializer_class = QuarterlyFilerViewSerializer
 

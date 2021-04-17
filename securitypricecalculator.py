41ceb7bb213c4e187ccc4fb25a358b88267c81fe
Melvin Mathew <melvin15may@gmail.com>
2021-04-17 19:47:47 -0400

    update price calculator job

--- securitypricecalculator.py
+++ securitypricecalculator.py
@@ -1,62 +1,57 @@
-import datetime
-import sys
+import getopt
 import os
+import sys
 import django
+import logging
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fedgehundapi.settings')
 django.setup()
-from next_prev import next_in_order, prev_in_order
-from edgar.models import QuarterlyHolding, QuarterlySecurityHolding, Filer, Security
-from security.models import Price
 
-quarterlyholdings_all = securities = quarterlysecurityholdings = quarterlysecurityholdings_all = []
-securities = Security.objects.all()
-filers = Filer.objects.all()
-quarterlyholdings_all = QuarterlyHolding.objects.all()
-quarterlysecurityholdings = QuarterlySecurityHolding.objects.all()
-quarters = [42019,12020,22020,32020,42020]
-#############################################################################################################################
-securities_securities=Security.objects.all()
-for security in securities_securities:
-    print(security.securityName)
-    quarterlysecurityholdingsforsecurity = QuarterlySecurityHolding.objects.filter(securityId=security)
-    # saare quarterly security holding for a security for all filers ke liye value divide by quantity
-    for quarter in quarters:
-        total_value = 0
-        total_quantity = 0
-        avg_value = 0
-        quarterlyholdingforquarter = QuarterlyHolding.objects.filter(quarter=quarter)
-        quarterlysecurityholdingsforsecurity = QuarterlySecurityHolding.objects.filter(securityId=security,quarterlyHoldingId__in =quarterlyholdingforquarter)
-        for quarterlysecurityholdingforsecurity in quarterlysecurityholdingsforsecurity:        
-            try:                
-                total_value = total_value + quarterlysecurityholdingforsecurity.marketvalue*1000
-            except:
-                print('Here')
-                pass
-            try:
-                total_quantity = total_quantity + quarterlysecurityholdingforsecurity.quantity
-            except:
-                print('Here')
-                pass
-        try:
-            if(total_quantity==0):
-                q = Price(securityId=security, value = None, quarter = quarter, cusip = quarterlysecurityholdingforsecurity.cusip, name = security.name)
-            else:
-                avg_value = total_value/total_quantity
-                try:
-                    print(security)
-                    print(avg_value)
-                    print(quarter)
-                    print(quarterlysecurityholdingforsecurity)
-                    q = Price(securityId=security, value = avg_value, quarter = quarter, cusip = '', name = security.securityName)
-                    print('Security Info Saved')
-                except:
-                    print('herrrr')
-                    pass
-            try:    
-                q.save()
-            except:
-                print('Problem while saving')
-                pass
-        except:
-            print('Problem while saving')
-            pass
+from django.db.models import Sum
+from edgar.models import QuarterlySecurityHolding, Security
+
+logger = logging.getLogger("positions_job")
+
+
+def bulk_get_or_create(prices):
+    logger.info("%1s", prices)
+
+
+def calculate_price(security):
+    totals_per_quarter = QuarterlySecurityHolding.objects.filter(securityId=security).values(
+        'quarterlyHoldingId__quarter').annotate(
+        total_market_value=Sum('marketvalue'), total_quantity=Sum('quantity'))
+
+    prices = []
+    for total_per_quarter in totals_per_quarter:
+        quarter = total_per_quarter.get('quarterlyHoldingId__quarter')
+        if quarter is None:
+            continue
+
+        total_value = total_per_quarter.get('total_market_value', 0)
+        if total_value is None or total_value == 0:
+            continue
+
+        total_quantity = total_per_quarter.get('total_quantity', 0)
+        if total_quantity is None or total_quantity == 0:
+            continue
+        price = {'securityId': security, 'value': total_value / total_quantity, 'quarter': quarter,
+                 'cusip': security.cusip, 'name': security.securityName}
+
+        prices.append(price)
+    bulk_get_or_create(prices)
+
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "n:", ["numThreads="])
+    except getopt.GetoptError:
+        print('positions_job.py -n <numberOfThreads>')
+        sys.exit(2)
+    n_jobs = 2
+    for opt, arg in opts:
+        if opt == '-n':
+            n_jobs = int(arg)
+
+    for security in Security.objects.all():
+        calculate_price(security)

aeb2f0c556584054af88cd1d649edbea7221fe49
Pushpit <pushpit07@gmail.com>
2021-05-21 18:05:12 +0530

    Updates positions_job to include   allowDiskUse=True  and formatted filer page tables
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- fedgehundui/src/components/Filer/Figures/BiggestHoldingsTable.js
+++ fedgehundui/src/components/Filer/Figures/BiggestHoldingsTable.js
@@ -32,40 +32,40 @@ function BiggestHoldingsTable(props) {
 
     if (typeof data !== 'undefined' && data.length > 0) {
         // Top 5 Biggest Holdings
-        var symbol1 = data[0]["ticker"];
-        var securityName1 = data[0]["securityName"];
-        var securityType1 = data[0]["type1"];
-        var price1 = Math.round(data[0]["lastPrice"]);
-        var marketValue1 = data[0]["marketValue"];
-        var weightPercent1 = data[0]["weightPercent"].toFixed(2) + " %";
+        var symbol1 = data[0]["ticker"] ? data[0]["ticker"] : "-";
+        var securityName1 = data[0]["securityName"] ? data[0]["securityName"] : "-";
+        var securityType1 = data[0]["type1"] ? data[0]["type1"] : "-";
+        var price1 = data[0]["lastPrice"] ? Math.round(data[0]["lastPrice"]) : "-";
+        var marketValue1 = data[0]["marketValue"] ? data[0]["marketValue"] : "-";
+        var weightPercent1 = data[0]["weightPercent"] ? data[0]["weightPercent"].toFixed(2) + " %" : "-";
 
-        var symbol2 = data[1]["ticker"];
-        var securityName2 = data[1]["securityName"];
-        var securityType2 = data[1]["type1"];
-        var price2 = Math.round(data[1]["lastPrice"]);
-        var marketValue2 = data[1]["marketValue"];
-        var weightPercent2 = data[1]["weightPercent"].toFixed(2) + " %";
+        var symbol2 = data[1]["ticker"] ? data[1]["ticker"] : "-";
+        var securityName2 = data[1]["securityName"] ? data[1]["securityName"] : "-";
+        var securityType2 = data[1]["type1"] ? data[1]["type1"] : "-";
+        var price2 = data[1]["lastPrice"] ? Math.round(data[1]["lastPrice"]) : "-";
+        var marketValue2 = data[1]["marketValue"] ? data[1]["marketValue"] : "-";
+        var weightPercent2 = data[1]["weightPercent"] ? data[1]["weightPercent"].toFixed(2) + " %" : "-";
 
-        var symbol3 = data[2]["ticker"];
-        var securityName3 = data[2]["securityName"];
-        var securityType3 = data[2]["type1"];
-        var price3 = Math.round(data[2]["lastPrice"]);
-        var marketValue3 = data[2]["marketValue"];
-        var weightPercent3 = data[2]["weightPercent"].toFixed(2) + " %";
+        var symbol3 = data[2]["ticker"] ? data[2]["ticker"] : "-";
+        var securityName3 = data[2]["securityName"] ? data[2]["securityName"] : "-";
+        var securityType3 = data[2]["type1"] ? data[2]["type1"] : "-";
+        var price3 = data[2]["lastPrice"] ? Math.round(data[2]["lastPrice"]) : "-";
+        var marketValue3 = data[2]["marketValue"] ? data[2]["marketValue"] : "-";
+        var weightPercent3 = data[2]["weightPercent"] ? data[2]["weightPercent"].toFixed(2) + " %" : "-";
 
-        var symbol4 = data[3]["ticker"];
-        var securityName4 = data[3]["securityName"];
-        var securityType4 = data[3]["type1"];
-        var price4 = Math.round(data[3]["lastPrice"]);
-        var marketValue4 = data[3]["marketValue"];
-        var weightPercent4 = data[3]["weightPercent"].toFixed(2) + " %";
+        var symbol4 = data[3]["ticker"] ? data[3]["ticker"] : "-";
+        var securityName4 = data[3]["securityName"] ? data[3]["securityName"] : "-";
+        var securityType4 = data[3]["type1"] ? data[3]["type1"] : "-";
+        var price4 = data[3]["lastPrice"] ? Math.round(data[3]["lastPrice"]) : "-";
+        var marketValue4 = data[3]["marketValue"] ? data[3]["marketValue"] : "-";
+        var weightPercent4 = data[3]["weightPercent"] ? data[3]["weightPercent"].toFixed(2) + " %" : "-";
 
-        var symbol5 = data[4]["ticker"];
-        var securityName5 = data[4]["securityName"];
-        var securityType5 = data[4]["type1"];
-        var price5 = Math.round(data[4]["lastPrice"]);
-        var marketValue5 = data[4]["marketValue"];
-        var weightPercent5 = data[4]["weightPercent"].toFixed(2) + " %";
+        var symbol5 = data[4]["ticker"] ? data[4]["ticker"] : "-";
+        var securityName5 = data[4]["securityName"] ? data[4]["securityName"] : "-";
+        var securityType5 = data[4]["type1"] ? data[4]["type1"] : "-";
+        var price5 = data[4]["lastPrice"] ? Math.round(data[4]["lastPrice"]) : "-";
+        var marketValue5 = data[4]["marketValue"] ? data[4]["marketValue"] : "-";
+        var weightPercent5 = data[4]["weightPercent"] ? data[4]["weightPercent"].toFixed(2) + " %" : "-";
     }
 
 
@@ -86,7 +86,7 @@ function BiggestHoldingsTable(props) {
                         </thead>
                         <tbody className="biggest_holdings_content">
                             <tr>
-                                <td>{symbol1}</td>
+                                <td className="left_table_symbol">{symbol1}</td>
                                 <td>{securityName1}</td>
                                 <td>{securityType1}</td>
                                 <td>{price1}</td>
@@ -94,7 +94,7 @@ function BiggestHoldingsTable(props) {
                                 <td>{weightPercent1}</td>
                             </tr>
                             <tr>
-                                <td>{symbol2}</td>
+                                <td className="left_table_symbol">{symbol2}</td>
                                 <td>{securityName2}</td>
                                 <td>{securityType2}</td>
                                 <td>{price2}</td>
@@ -102,7 +102,7 @@ function BiggestHoldingsTable(props) {
                                 <td>{weightPercent2}</td>
                             </tr>
                             <tr>
-                                <td>{symbol3}</td>
+                                <td className="left_table_symbol">{symbol3}</td>
                                 <td>{securityName3}</td>
                                 <td>{securityType3}</td>
                                 <td>{price3}</td>
@@ -110,7 +110,7 @@ function BiggestHoldingsTable(props) {
                                 <td>{weightPercent3}</td>
                             </tr>
                             <tr>
-                                <td>{symbol4}</td>
+                                <td className="left_table_symbol">{symbol4}</td>
                                 <td>{securityName4}</td>
                                 <td>{securityType4}</td>
                                 <td>{price4}</td>
@@ -118,7 +118,7 @@ function BiggestHoldingsTable(props) {
                                 <td>{weightPercent4}</td>
                             </tr>
                             <tr>
-                                <td>{symbol5}</td>
+                                <td className="left_table_symbol">{symbol5}</td>
                                 <td>{securityName5}</td>
                                 <td>{securityType5}</td>
                                 <td>{price5}</td>
--- fedgehundui/src/components/Filer/Figures/TopBuysTable.js
+++ fedgehundui/src/components/Filer/Figures/TopBuysTable.js
@@ -35,35 +35,35 @@ function TopBuysTable(props) {
     // Change this according to TopBuys
     if (typeof data !== 'undefined' && data.length > 0) {
         // Top Buys of the Quarter
-        var topBuySymbol1 = data[0]["ticker"];
-        var topBuySecurityName1 = data[0]["securityName"];
-        var topBuySecurityType1 = data[0]["type1"];
-        var topBuyMarketValue1 = data[0]["marketValue"];
-        var topBuychangePercent1 = data[0]["changeInPositionPercent"].toFixed(2) + " %";;
+        var topBuySymbol1 = data[0]["ticker"] ? data[0]["ticker"] : "-";
+        var topBuySecurityName1 = data[0]["securityName"] ? data[0]["securityName"] : "-";
+        var topBuySecurityType1 = data[0]["type1"] ? data[0]["type1"] : "-";
+        var topBuyMarketValue1 = data[0]["marketValue"] ? data[0]["marketValue"] : "-";
+        var topBuychangePercent1 = ((data[0]["changeInPositionPercent"]) ? (data[0]["changeInPositionPercent"].toFixed(2) + " %") : "-");
 
-        var topBuySymbol2 = data[1]["ticker"];
-        var topBuySecurityName2 = data[1]["securityName"];
-        var topBuySecurityType2 = data[1]["type1"];
-        var topBuyMarketValue2 = data[1]["marketValue"];
-        var topBuychangePercent2 = data[1]["changeInPositionPercent"].toFixed(2) + " %";;
+        var topBuySymbol2 = data[1]["ticker"] ? data[1]["ticker"] : "-";
+        var topBuySecurityName2 = data[1]["securityName"] ? data[1]["securityName"] : "-";
+        var topBuySecurityType2 = data[1]["type1"] ? data[1]["type1"] : "-";
+        var topBuyMarketValue2 = data[1]["marketValue"] ? data[1]["marketValue"] : "-";
+        var topBuychangePercent2 = ((data[1]["changeInPositionPercent"]) ? (data[1]["changeInPositionPercent"].toFixed(2) + " %") : "-");
 
-        var topBuySymbol3 = data[2]["ticker"];
-        var topBuySecurityName3 = data[2]["securityName"];
-        var topBuySecurityType3 = data[2]["type1"];
-        var topBuyMarketValue3 = data[2]["marketValue"];
-        var topBuychangePercent3 = data[2]["changeInPositionPercent"].toFixed(2) + " %";;
+        var topBuySymbol3 = data[2]["ticker"] ? data[2]["ticker"] : "-";
+        var topBuySecurityName3 = data[2]["securityName"] ? data[2]["securityName"] : "-";
+        var topBuySecurityType3 = data[2]["type1"] ? data[2]["type1"] : "-";
+        var topBuyMarketValue3 = data[2]["marketValue"] ? data[2]["marketValue"] : "-";
+        var topBuychangePercent3 = ((data[2]["changeInPositionPercent"]) ? (data[2]["changeInPositionPercent"].toFixed(2) + " %") : "-");
 
-        var topBuySymbol4 = data[3]["ticker"];
-        var topBuySecurityName4 = data[3]["securityName"];
-        var topBuySecurityType4 = data[3]["type1"];
-        var topBuyMarketValue4 = data[3]["marketValue"];
-        var topBuychangePercent4 = data[3]["changeInPositionPercent"].toFixed(2) + " %";;
+        var topBuySymbol4 = data[3]["ticker"] ? data[3]["ticker"] : "-";
+        var topBuySecurityName4 = data[3]["securityName"] ? data[3]["securityName"] : "-";
+        var topBuySecurityType4 = data[3]["type1"] ? data[3]["type1"] : "-";
+        var topBuyMarketValue4 = data[3]["marketValue"] ? data[3]["marketValue"] : "-";
+        var topBuychangePercent4 = ((data[3]["changeInPositionPercent"]) ? (data[3]["changeInPositionPercent"].toFixed(2) + " %") : "-");
 
-        var topBuySymbol5 = data[4]["ticker"];
-        var topBuySecurityName5 = data[4]["securityName"];
-        var topBuySecurityType5 = data[4]["type1"];
-        var topBuyMarketValue5 = data[4]["marketValue"];
-        var topBuychangePercent5 = data[4]["changeInPositionPercent"].toFixed(2) + " %";;
+        var topBuySymbol5 = data[4]["ticker"] ? data[4]["ticker"] : "-";
+        var topBuySecurityName5 = data[4]["securityName"] ? data[4]["securityName"] : "-";
+        var topBuySecurityType5 = data[4]["type1"] ? data[4]["type1"] : "-";
+        var topBuyMarketValue5 = data[4]["marketValue"] ? data[4]["marketValue"] : "-";
+        var topBuychangePercent5 = ((data[4]["changeInPositionPercent"]) ? (data[4]["changeInPositionPercent"].toFixed(2) + " %") : "-");
     }
 
     return (
@@ -82,35 +82,35 @@ function TopBuysTable(props) {
                         </thead>
                         <tbody className="top_buys_cells_content">
                             <tr>
-                                <td>{topBuySymbol1}</td>
+                                <td className="top_buys_left_table_symbol">{topBuySymbol1}</td>
                                 <td>{topBuySecurityName1}</td>
                                 <td>{topBuySecurityType1}</td>
                                 <td>{topBuyMarketValue1}</td>
                                 <td>{topBuychangePercent1}</td>
                             </tr>
                             <tr>
-                                <td>{topBuySymbol2}</td>
+                                <td className="top_buys_left_table_symbol">{topBuySymbol2}</td>
                                 <td>{topBuySecurityName2}</td>
                                 <td>{topBuySecurityType2}</td>
                                 <td>{topBuyMarketValue2}</td>
                                 <td>{topBuychangePercent2}</td>
                             </tr>
                             <tr>
-                                <td>{topBuySymbol3}</td>
+                                <td className="top_buys_left_table_symbol">{topBuySymbol3}</td>
                                 <td>{topBuySecurityName3}</td>
                                 <td>{topBuySecurityType3}</td>
                                 <td>{topBuyMarketValue3}</td>
                                 <td>{topBuychangePercent3}</td>
                             </tr>
                             <tr>
-                                <td>{topBuySymbol4}</td>
+                                <td className="top_buys_left_table_symbol">{topBuySymbol4}</td>
                                 <td>{topBuySecurityName4}</td>
                                 <td>{topBuySecurityType4}</td>
                                 <td>{topBuyMarketValue4}</td>
                                 <td>{topBuychangePercent4}</td>
                             </tr>
                             <tr>
-                                <td>{topBuySymbol5}</td>
+                                <td className="top_buys_left_table_symbol">{topBuySymbol5}</td>
                                 <td>{topBuySecurityName5}</td>
                                 <td>{topBuySecurityType5}</td>
                                 <td>{topBuyMarketValue5}</td>
